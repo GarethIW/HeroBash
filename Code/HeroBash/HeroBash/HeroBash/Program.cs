4ef38e7efe2642a9459c4719b7f1c6f0d497beb0
Gareth Williams <gareth@garethw.net>
2013-01-02 13:46:50 +0000

    First Commit

---
+++ Code/HeroBash/HeroBash/HeroBash/Program.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace HeroBash
+{
+#if WINDOWS || XBOX 
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            using (HeroBash game = new HeroBash())
+            {
+                game.Run();
+            }
+        }
+    }
+#endif
+#if WINRT
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            var factory = new MonoGame.Framework.GameFrameworkViewSource<HeroBash>();
+            Windows.ApplicationModel.Core.CoreApplication.Run(factory);
+        }
+    }
+#endif
+}
+
